acc94923fd9872583470a780233cc9f4482df116
Anna He <130006361+duomei98@users.noreply.github.com>
2024-10-11 14:43:08 -0700

    Move lab2.html and my-app to new repo

---
+++ my-app/src/constants.ts
@@ -0,0 +1,39 @@
+import { Label } from "./types";
+export const dummyNotesList = [
+   {
+       id: 1,
+       title: "FNC vs WBG",
+       content: "10/12: 5am",
+       label: Label.other,
+   },
+   {
+       id: 2,
+       title: "TL vs GAM",
+       content: "10/12: 8am",
+       label: Label.other,
+   },
+   {
+       id: 3,
+       title: "CSE 107 HW2",
+       content: "10/13; Midnight",
+       label: Label.study,
+   },
+   {
+       id: 4,
+       title: "Coding Assessment",
+       content: "10/13; Midnight",
+       label: Label.work,
+   },
+   {
+       id: 5,
+       title: "CSE 150A HW2",
+       content: "10/15; Midnight",
+       label: Label.study,
+   },
+   {
+       id: 6,
+       title: "CSE 107 Quiz",
+       content: "10/16; Class",
+       label: Label.study,
+   },
+]
